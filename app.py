ac8320852af3e55a6e89ea77344ce97977fc99cc
Sang-19 <sangbedpati1234@gmail.com>
2026-09-03 18:21:07 +0530

    complete

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 import requests
 import streamlit as st
-from streamlit_option_menu import option_menu
+
 import gspread
 st.set_page_config(layout="wide")
 
@@ -11,7 +11,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-color: #000000;
+        background-color: #0000;
     }
     
     </style>
@@ -70,8 +70,13 @@ iframe {
 }
 
 @keyframes slide {
-    from { transform: translateX(0); }
-    to { transform: translateX(-100%); }
+    0% {
+        transform: translateX(0);
+    }
+
+    100% {
+        transform: translateX(-100%);
+    }
 }
 </style>
 
@@ -114,10 +119,50 @@ iframe {
         <div class="card"><iframe src="https://www.instagram.com/reel/DZotNbxTuzf/embed"></iframe></div>
         <div class="card"><iframe src="https://www.instagram.com/reel/DZjf0m0zW9S/embed"></iframe></div>
         <div class="card"><iframe src="https://www.instagram.com/reel/DZaQfB3zXEF/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/p/DctXaAkSloW/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/p/DcsLFmtziRb/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/p/DcqnokvTICk/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/p/Dcdp9dDJOiA/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DcsLFmtziRb/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DcZ54-Yhokd/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/Db5l4cjE_lx/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/Dbs0iFfIznG/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DbZ2wwYxYJK/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DbGGJWipfhv/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DbDwS4DvZhF/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DbAAi4XBjKx/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/Dar73G-JoUO/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DaSDk6MMP5u/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DaC35hxzqox/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DZ5ajULT15g/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DZuoGVyTODx/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DZthf9szO1r/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DZotNbxTuzf/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DZjf0m0zW9S/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DZaQfB3zXEF/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/Db5l4cjE_lx/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/Dbs0iFfIznG/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DbZ2wwYxYJK/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DbGGJWipfhv/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DbDwS4DvZhF/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DbAAi4XBjKx/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/Dar73G-JoUO/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DaSDk6MMP5u/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DaC35hxzqox/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DZ5ajULT15g/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DZuoGVyTODx/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DZthf9szO1r/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DZotNbxTuzf/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DZjf0m0zW9S/embed"></iframe></div>
+        <div class="card"><iframe src="https://www.instagram.com/reel/DZaQfB3zXEF/embed"></iframe></div>
+
     </div>
 </div>
 """, height=320)
 
+def spacce():
+       st.write("")
+       st.write("")
 def contact():
     credentials = st.secrets["gcp_service_account"]
     gc = gspread.service_account_from_dict(credentials)
@@ -125,21 +170,25 @@ def contact():
     left, right = st.columns(2)
     with left:
            st.title("Contact me")
-           st.write("Feel Free to reach out by fiulling out the form below")
+           st.write("Feel Free to reach out:-")
     with right:
            with st.form("Contact form"):
                 name = st.text_input("Your Name")
                 email = st.text_input("Your Email Address")
+                number = st.text_input("Your contact number")
                 message = st.text_area("Your Message")
+                
                 submit_button = st.form_submit_button("Send Message")
                 if submit_button:
                       if not name or not email or not message:
                               st.warning("Please fill out all fields bfore submiting")
                       else:
-                             sheet.append_row([name, email, message])
+                             sheet.append_row([name, email, message, number])
                              st.success(f"Thank you {name}! Your message has been sent")
                        
 
 home()
+spacce()
 instagram()
+spacce()
 contact()
